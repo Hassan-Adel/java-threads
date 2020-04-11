090912804a27c573d8a582ae9644eb6b1aa3a903
hassan.adel <hasanadel555@hotmail.com>
2020-04-11 04:55:35 +0200

    Processes and Threads

--- src/com/tutorial/ThreadsDemo.java
+++ src/com/tutorial/ThreadsDemo.java
@@ -1,4 +1,8 @@
 package com.tutorial;
 
 public class ThreadsDemo {
+    public static void showCount(){
+        System.out.println(Thread.activeCount());
+        System.out.println(Runtime.getRuntime().availableProcessors());
+    }
 }
