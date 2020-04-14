498ff38497b6bf81a01ae705407799cd66536626
hassan.adel <hasanadel555@hotmail.com>
2020-04-14 03:32:04 +0200

    Race Condition Fix 1 : Confinement

--- src/com/tutorial/threads/ThreadsDemo.java
+++ src/com/tutorial/threads/ThreadsDemo.java
@@ -1,5 +1,8 @@
 package com.tutorial.threads;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ThreadsDemo {
     public static void showCount(){
         System.out.println(Thread.activeCount());
@@ -40,4 +43,53 @@ public class ThreadsDemo {
         }
         thread.interrupt();
     }
+
+    public static void raceCondition(){
+        var status = new DownloadStatus();
+        List<Thread> threads = new ArrayList<>();
+        for (int i = 0; i<10 ; i++){
+            var thread = new Thread(new DownloadFileTask(status));
+            thread.start();
+            threads.add(thread);
+        }
+
+        for (var thread:threads){
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println(status.getTotalBytes());
+
+    }
+
+    public static void Confinement(){
+        List<Thread> threads = new ArrayList<>();
+        List<DownloadFileTask> tasks = new ArrayList<>();
+
+        for (int i = 0; i<10 ; i++){
+            var task = new DownloadFileTask();
+            tasks.add(task);
+
+            var thread = new Thread(task);
+            thread.start();
+            threads.add(thread);
+        }
+
+        for (var thread:threads){
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        var totalBytes = tasks.stream().map(t -> t.getStatus().getTotalBytes()).reduce(0, (a, b) -> a+b);
+
+        System.out.println(totalBytes);
+
+    }
+
 }
