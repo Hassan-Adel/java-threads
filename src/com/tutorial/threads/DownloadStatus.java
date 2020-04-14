a6cd43482385d527496ac87d77995bfa78ede79f
hassan.adel <hasanadel555@hotmail.com>
2020-04-14 07:07:52 +0200

    Race Condition Fix 2 : The synchronized Keyword

--- src/com/tutorial/threads/DownloadStatus.java
+++ src/com/tutorial/threads/DownloadStatus.java
@@ -4,12 +4,35 @@ import java.util.concurrent.atomic.LongAdder;
 
 public class DownloadStatus {
   private int totalBytes;
+  private int totalFiles;
+  private Object totalBytesLock = new Object();
+  private Object totalFilesLock = new Object();
+
+
   public int getTotalBytes() {
     return totalBytes;
   }
 
   public void incrementTotalBytes() {
-    this.totalBytes++;
+    //every java object has a built in lock (monitor object)
+    synchronized (totalBytesLock){
+      this.totalBytes++;
+    }
   }
+
+  public int getTotalFiles() {
+    return totalFiles;
+  }
+
+  public void incrementTotalFiles() {
+    synchronized (totalFilesLock){
+      this.totalFiles++;
+    }
+  }
+
+    //same as using synchronized(this)
+    public synchronized void incrementTotalFilesByTwo(){
+        this.totalFiles+=2;
+    }
 
 }
