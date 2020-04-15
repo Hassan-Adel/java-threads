27af794bc606f255e60d6ec2b216bb0dfee8bfa0
hassan.adel <hasanadel555@hotmail.com>
2020-04-15 03:02:31 +0200

    The volatile Keyword : Fix visibility problem

--- src/com/tutorial/threads/DownloadFileTask.java
+++ src/com/tutorial/threads/DownloadFileTask.java
@@ -5,21 +5,33 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class DownloadFileTask implements Runnable {
+
+
+    private DownloadStatus status;
+
+    public DownloadFileTask(DownloadStatus status) {
+        this.status = status;
+    }
+
+    public DownloadFileTask() {
+        this.status = new DownloadStatus();
+    }
+
     @Override
     public void run() {
         String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
         System.out.println("Downloading a file : " + Thread.currentThread().getName() + " - " + timeStamp);
-//        try {
-//            Thread.sleep(5000);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-        for (int i = 0 ; i<Integer.MAX_VALUE; i++) {
+        for (int i = 0 ; i<10_0000; i++) {
             //check for an interruption request
             if (Thread.currentThread().isInterrupted())
                 return;
-            System.out.println("Downloading byte " + i);
+            status.incrementTotalBytes();
         }
+        status.done();
         System.out.println("Download completed: " + Thread.currentThread().getName());
+    }
+
+    public DownloadStatus getStatus() {
+        return status;
     }
 }
